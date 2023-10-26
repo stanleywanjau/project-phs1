b18ca3bd3633b90462574c4674aac04b2ca985d4
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-25 20:39:59 -0700

    fetched data from my prefered api and displayed it

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-document.addEventListener('DOMContentLoaded', bookReview)
-
+document.addEventListener('DOMContentLoaded', ()=>{
+const bookReview = document.getElementById('book-review')
+const booklist = document.getElementById("book-list")
 
 fetch ('https://www.googleapis.com/books/v1/volumes?q=friction')
-.then(res=>res.json)
-.then(data=>displayBook(data))
+.then(res=>res.json())
+.then(data=>data.items.forEach(book=> {
+    const bookInfo = book.volumeInfo;
+    const bookItem =document.createElement('div') 
+    bookItem.classList.add('book');
+    bookItem.innerHTML=`
+    <h2>${bookInfo.title}</h2>
+    <img src="${bookInfo.imageLinks.thumbnail}" alt="book Cover">
+    <p><strong>AUTHOR:</strong>${bookInfo.authors}</p>
+    <p><strong>publisher:</strong>${bookInfo.publisher}</p>
+    <p><strong>publishedDate</strong>${bookInfo.publishedDate}</p>
+    <p><strong>description</strong>${bookInfo.description}</p>
+    
+    `;
 
-function displayBook(book){
-    const booklist=document.querySelector('#book-list')
-    const 
+    const bookListItem = document.createElement("li")
+    bookListItem.classList.add('book')
+    bookListItem.innerHTML=`
+    <h5>${bookInfo.title}</h5>
+    `
+    bookReview.appendChild(bookItem);
+    booklist.appendChild(bookListItem)
 
-}
+    
+})
+)
+
+    
+
+    }
+    
+    
+
+)
