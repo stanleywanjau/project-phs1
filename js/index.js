1b84ca0a063c1344baa8e7fcf3e249463412ee6b
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-26 14:30:09 -0700

    changed the like button

--- js/index.js
+++ js/index.js
@@ -1,78 +1,59 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-const bookReview = document.getElementById('book-review')
-const booklist = document.getElementById("book-list")
-
-fetch ('https://www.googleapis.com/books/v1/volumes?q=friction')
-.then(res=>res.json())
-.then(data=>data.items.forEach(book=> {
-    //create where to get the book information
-    const bookInfo = book.volumeInfo;
-    const bookItem =document.createElement('div') 
-    bookItem.classList.add('book');
-    bookItem.innerHTML=`
-    <h2>${bookInfo.title}</h2>
-    <img src="${bookInfo.imageLinks.thumbnail}" alt="book Cover">
-    <p><strong>AUTHOR:</strong>${bookInfo.authors}</p>
-    <p><strong>publisher:</strong>${bookInfo.publisher}</p>
-    <p><strong>publishedDate</strong>${bookInfo.publishedDate}</p>
-    <p><strong>description</strong>${bookInfo.description}</p>
-    <button id="likeButton" class="like-button">Like</button>
-    <p id="likeCount">0</p>
-    <div id="comments">
-    <!-- Existing comments will be displayed here -->
-</div>
-<form id="commentForm">
-    <textarea id="commentText" placeholder="Write your comment..."></textarea>
-    <button type="submit">Post Comment</button>
-</form>
-       
-   </ul>
+document.addEventListener('DOMContentLoaded', () => {
+    const bookReview = document.getElementById('book-review');
+    const booklist = document.getElementById('book-list');
+  
+    fetch('https://www.googleapis.com/books/v1/volumes?q=friction')
+      .then((res) => res.json())
+      .then((data) => {
+        data.items.forEach((book) => {
+          const bookInfo = book.volumeInfo;
+          const bookItem = document.createElement('div');
+          bookItem.classList.add('book');
+          bookItem.innerHTML = `
+            <h2>${bookInfo.title}</h2>
+            <img src="${bookInfo.imageLinks.thumbnail}" alt="book Cover">
+            <p><strong>AUTHOR:</strong>${bookInfo.authors}</p>
+            <p><strong>publisher:</strong>${bookInfo.publisher}</p>
+            <p><strong>publishedDate</strong>${bookInfo.publishedDate}</p>
+            <p><strong>description</strong>${bookInfo.description}</p>
+            <button class="like-button">Like</button>
+            <p class="likeCount">0</p>
+            <div class="comments">
+              <!-- Existing comments will be displayed here -->
+            </div>
+            <form class="commentForm">
+              <textarea class="commentText" placeholder="Write your comment..."></textarea>
+              <button type="submit">Post Comment</button>
+            </form>
+          `;
+  
+          const bookListItem = document.createElement('li');
+          bookListItem.classList.add('book');
+          bookListItem.innerHTML = `
+            <h5>${bookInfo.title}</h5>
+          `;
+  
+          bookReview.appendChild(bookItem);
+          booklist.appendChild(bookListItem);
+        });
+        
+        // Function to handle the click event of the like button
+        function like(event) {
+          let likeCount = parseInt(event.target.nextElementSibling.textContent);
+          likeCount += 1;
+          event.target.nextElementSibling.textContent = likeCount;
+        }
+  
+        // Add an event listener for the like button
+        const likeButtons = document.querySelectorAll('.like-button');
+        likeButtons.forEach((button) => {
+          button.addEventListener('click', like);
+        });
+  
    
-    
-    `;
-// create list of books
-    const bookListItem = document.createElement("li")
-    bookListItem.classList.add('book')
-    bookListItem.innerHTML=`
-    <h5>${bookInfo.title}</h5>
-    `
-    bookReview.appendChild(bookItem);
-    booklist.appendChild(bookListItem);
-
-
-function search_book(bookInfo){
-let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementById("book-list"); 
-      
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="book-item";                  
-        } 
-        return search
-    } 
-}
-let likeCount = 0;
-
-// Function to handle the click event of the like button
-function like() {
-    likeCount+=1;
-    document.getElementById("likeCount").textContent = likeCount;
-}
-
-// Add an event listener to the like button
-document.getElementById("likeButton").addEventListener("click", like)
-    
-})
-)
-
-    
-
-    }
-    
-    
-
-)
+        });
+  
+       
+       
+  });
+  
