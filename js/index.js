4118e65e67a443fac2d177eed868d84da5b75c63
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-25 21:59:48 -0700

    added a search bar

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const booklist = document.getElementById("book-list")
 fetch ('https://www.googleapis.com/books/v1/volumes?q=friction')
 .then(res=>res.json())
 .then(data=>data.items.forEach(book=> {
+    //create where to get the book information
     const bookInfo = book.volumeInfo;
     const bookItem =document.createElement('div') 
     bookItem.classList.add('book');
@@ -16,16 +17,34 @@ fetch ('https://www.googleapis.com/books/v1/volumes?q=friction')
     <p><strong>publishedDate</strong>${bookInfo.publishedDate}</p>
     <p><strong>description</strong>${bookInfo.description}</p>
     
+   
+    
     `;
-
+// create list of books
     const bookListItem = document.createElement("li")
     bookListItem.classList.add('book')
     bookListItem.innerHTML=`
     <h5>${bookInfo.title}</h5>
     `
     bookReview.appendChild(bookItem);
-    booklist.appendChild(bookListItem)
+    booklist.appendChild(bookListItem);
 
+
+function search_book(bookInfo){
+let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementById("book-list"); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="book-item";                  
+        } 
+        return search
+    } 
+}
     
 })
 )
